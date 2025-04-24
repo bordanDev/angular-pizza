c355a7dae3783f613604392382d554b6bcae7094
bordanDev <bordanproduction@gmail.com>
2025-04-24 16:15:42 +0300

    add pizza to local storage & drawer component template

---
+++ src/app/ui/drawer/drawer.component.ts
@@ -0,0 +1,38 @@
+import { Component, effect, inject, signal, TemplateRef, ViewChild } from '@angular/core';
+import { AddPizzaService } from "../../features/pizza/services/add-pizza.service";
+import { Pizza } from "../../shared/interfaces/pizza.interface";
+import { IconSize } from "../icon/enums/icon.enums";
+import { Router } from "@angular/router";
+import { Location } from "@angular/common";
+
+@Component({
+  selector: 'app-drawer',
+  templateUrl: './drawer.component.html',
+  styleUrl: './drawer.component.scss',
+  host: {
+    '[style.justify-content]': 'drawerPizza() ? "space-between" : "center"'
+  }
+})
+export class DrawerComponent {
+
+  @ViewChild(TemplateRef) drawer: TemplateRef<unknown> | undefined;
+  @ViewChild(TemplateRef) drawerEmpty: TemplateRef<unknown> | undefined;
+
+  drawerPizzaData = inject(AddPizzaService);
+
+  constructor(private location: Location) {
+    effect(() => {
+      this.drawerPizza.set(this.drawerPizzaData.localSignalStorage())
+    }, {allowSignalWrites: true});
+  }
+
+  drawerPizza = signal<Pizza[]>([])
+
+  totalPrice: number = 0
+
+  closeDrawer(){
+    this.location.back()
+  }
+
+  protected readonly IconSize = IconSize;
+}
